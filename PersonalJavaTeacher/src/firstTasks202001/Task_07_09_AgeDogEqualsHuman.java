cb9d7830b802cfc51d49cd7e258c91911da1a70c
Mikhail Safonov <ms33@inbox.ru>
2020-01-12 20:49:56 +0300

    Task_07_09_AgeDogEqualsHuman

---
+++ PersonalJavaTeacher/src/firstTasks202001/Task_07_09_AgeDogEqualsHuman.java
@@ -0,0 +1,18 @@
+package firstTasks202001;
+
+import java.util.Scanner;
+
+public class Task_07_09_AgeDogEqualsHuman {
+	public static void main(String[] args) {
+		int ageDog;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Введите возраст собаки, (полных лет):");
+		ageDog = in.nextInt();
+		in.close();
+		if (ageDog <= 2) {
+			System.out.println("Возраст соответствутет человеческому, лет: " + ageDog * 10.5);
+		} else {
+			System.out.println("Возраст соответствутет человеческому, лет: " + ((ageDog - 2) * 4 + 10.5 * 2));
+		}
+	}
+}
