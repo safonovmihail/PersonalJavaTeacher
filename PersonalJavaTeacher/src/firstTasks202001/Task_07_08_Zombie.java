66cfca4fab00647c94535a8b7f6ab91594a97da1
Mikhail Safonov <ms33@inbox.ru>
2020-01-12 20:35:05 +0300

    Task_07_08_Zombie

---
+++ PersonalJavaTeacher/src/firstTasks202001/Task_07_08_Zombie.java
@@ -0,0 +1,19 @@
+package firstTasks202001;
+
+import java.util.Scanner;
+
+public class Task_07_08_Zombie {
+
+	public static void main(String[] args) {
+		int hid;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Введите идентификатор: ");
+		hid = in.nextInt();
+		in.close();
+		if (hid % 5 == 0 & hid % 11 == 0) {
+			System.out.println("Перед вами человек!");
+		} else {
+			System.out.println("Огонь!!! Это зомби!!!");
+		}
+	}
+}
