a3b567c027b19ecd26e8acf6f1b331aa29482054
Mikhail Safonov <ms33@inbox.ru>
2020-01-20 11:29:08 +0300

    Task_08_03 Без [iter]

---
+++ PersonalJavaTeacher/src/secondTask20200114/Task_08_03_SumIntervalAverage_02_WithoutIter.java
@@ -0,0 +1,25 @@
+package secondTask20200114;
+
+import java.util.Scanner;
+
+public class Task_08_03_SumIntervalAverage_02_WithoutIter {
+
+	public static void main(String[] args) {
+			int i, n, m, sum, iter;
+			Scanner in = new Scanner(System.in);
+			System.out.print("Введите левую границу интервала [n]: ");
+			n = in.nextInt();
+			System.out.print("Введите правую границу интервала [m]: ");
+			m = in.nextInt();
+			in.close();
+			sum = 0;
+//			iter = 0;
+			for (i = n; i <= m; i++) {
+				sum = sum + i;
+//				iter ++;
+			}
+			System.out.println("Сумма целых чисел в интервале [n;m]: " + sum);
+//			System.out.println("Среднее арифметическое равно: " + (float)sum/(float)iter);
+			System.out.println("Среднее арифметическое равно: " + (float)sum/(float)(m-n+1));
+		}
+	}
