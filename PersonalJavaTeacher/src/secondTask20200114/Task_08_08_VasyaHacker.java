65a5a65e432b526c02e71da4d5653c80c2f72edf
Mikhail Safonov <ms33@inbox.ru>
2020-01-20 16:42:37 +0300

    Task_08-08_v01: with [Math.log]

--- PersonalJavaTeacher/src/secondTask20200114/Task_08_08_VasyaHacker.java
+++ PersonalJavaTeacher/src/secondTask20200114/Task_08_08_VasyaHacker.java
@@ -13,16 +13,17 @@ import java.util.Scanner;
 public class Task_08_08_VasyaHacker {
 
 	public static void main(String[] args) {
-		double n;
+		int n;
 		System.out.println("Введите число: ");
 		Scanner in = new Scanner(System.in);
-		n = in.nextDouble();
+		n = in.nextInt();
+		in.close();
 		double log = Math.log(n) / Math.log(2);
 		System.out.println("Степень числа 2: " + log);
 // проверяем, является ли получившаяся степень целым числом
 // [Вопрос] уверен, что есть более изящные способы проверки числа на принадлежность к 
 // целому типу. Подскажите, пожалуйста, направление, я попробую догадаться.	
-		if ((int) log == log) {
+		if (n %2 == 0) {
 			System.out.println("Служебное число");
 		} else {
 			System.out.println("Число не служебное");
