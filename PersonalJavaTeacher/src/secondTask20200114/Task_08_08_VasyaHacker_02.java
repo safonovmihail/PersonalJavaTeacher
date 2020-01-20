75be8d6287b0f92e952a9a9dfe2fec6e72fe3c51
Mikhail Safonov <ms33@inbox.ru>
2020-01-20 14:21:25 +0300

    08.08: вывод в конце программы

--- PersonalJavaTeacher/src/secondTask20200114/Task_08_08_VasyaHacker_02.java
+++ PersonalJavaTeacher/src/secondTask20200114/Task_08_08_VasyaHacker_02.java
@@ -8,25 +8,30 @@ package secondTask20200114;
 // степень двойки -- 1,2,4,8,16,32,...
 // Помогите Васе определить, будет ли некоторое целое положительное число, 
 // вводимое в программу, бинарным.
+
 import java.util.Scanner;
 
 public class Task_08_08_VasyaHacker_02 {
 
 	public static void main(String[] args) {
 		int n, i;
+		String msg;
 
 		System.out.println("Введите число: ");
 		Scanner in = new Scanner(System.in);
 		n = in.nextInt();
 		in.close();
 
+		msg = null;
+
 		for (i = 2; (i <= n); i = 2 * i) {
 			if (n % i != 0) {
-				System.out.println("Число не служебное");
+				msg = "Число не служебное";
 				break;
 			} else if (n / i == 1) {
-				System.out.println("Служебное число");
+				msg = "Служебное число";
 			}
 		}
+		System.out.println(msg);
 	}
 }
